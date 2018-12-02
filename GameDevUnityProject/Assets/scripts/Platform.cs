91a2dabc7026cbacc41cacec7ac241873185ce9d
eric556 <enpie123@gmail.com>
2018-12-02 00:04:56 -0600

    Added jumping and fixed platforms so that they hold onto objects as they move

--- GameDevUnityProject/Assets/scripts/Platform.cs
+++ GameDevUnityProject/Assets/scripts/Platform.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class Platform : MonoBehaviour {
+    Dictionary<string, Transform> parentDict = new Dictionary<string, Transform>();
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,4 +19,19 @@ public class Platform : MonoBehaviour {
 	}
 
     //TODO: character movement capture here
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        parentDict.Add(col.gameObject.name, col.gameObject.transform.parent);
+        col.gameObject.transform.parent = this.transform;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (parentDict.ContainsKey(col.gameObject.name))
+        {
+            col.gameObject.transform.parent = parentDict[col.gameObject.name];
+            parentDict.Remove(col.gameObject.name);
+        }
+    }
 }
--- GameDevUnityProject/Assets/scripts/fatkid_controller_script.cs
+++ GameDevUnityProject/Assets/scripts/fatkid_controller_script.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class fatkid_controller_script : MonoBehaviour {
 	public float maxSpeed;
 	bool moving;
+    bool grounded = false;
+	bool jumping = true;
 	public bool facingRight = true;
+    public float jumpForce = 200.0f;
+    public float footRayLength = 5.0f;
 	Rigidbody2D rb;
 	Animator anim;
 
@@ -31,6 +35,30 @@ public class fatkid_controller_script : MonoBehaviour {
 			flip ();
 		}else if(move < 0 && facingRight) {
 			flip();
+		}
+
+        
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, footRayLength, LayerMask.GetMask("ground"));
+
+        Debug.DrawRay(transform.position, -Vector2.up * footRayLength, Color.red);
+        if (hit.collider)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+		if (Input.GetButton ("Jump")) {
+            print(grounded);
+			if (!jumping && grounded) {
+				rb.AddForce (new Vector2 (0, jumpForce));
+                jumping = true;
+            }
+        } else {
+			jumping = false;
 		}
 	}
 
